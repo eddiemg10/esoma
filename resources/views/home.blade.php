42f1f32d6f7b0b40eb61038f52002f54af8096f2
eddiemg10 <59659920+eddiemg10@users.noreply.github.com>
2022-04-25 21:27:16 +0300

    Added JQuery

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/css/app.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
+
 <body>
-<h1 class="bg-red-200 p-8">Hello worlds</h1>
-    
+    <div class="flex justify-center items-center">
+        <h1 class="bg-red-200 p-8 w-full">Click Me</h1>
+
+    </div>
+
 </body>
+
 </html>
+
+<script>
+$("h1").click(function() {
+    alert("JQUERY Connected");
+});
+</script>
