6497480ad2a843ab26f64506ab07ab3329fa0cc7
eddiemg10 <59659920+eddiemg10@users.noreply.github.com>
2022-05-31 21:19:23 +0300

    Dashboard-classroom and school data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,10 +14,22 @@ class DashboardController extends Controller
 
     public function index(){
 
-        $user = 1;
-        $classrooms = ClassroomStudent::where('user_id', $user)->count();
-        return $classrooms;
-        $data = [];
+        $user = 2;
+        $classrooms = DB::table('classroom_student')
+                        ->where('user_id', $user)
+                        ->join('classrooms', 'classroom_student.id', '=', 'classrooms.id')
+                        ->select('classrooms.*')
+                        ->get();
+
+        $schools = DB::table('classroom_student')
+                      ->join('school_classroom', 'classroom_student.classroom_id', '=', 'school_classroom.classroom_id')
+                      ->groupBy('school_id')
+                      ->get();
+
+        $data = [
+            "classrooms" => $classrooms,
+            "schools" => $schools
+        ];
         return view('dashboard/dashboard', $data);
     }
 }
