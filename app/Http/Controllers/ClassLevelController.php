aad8149f3cf383ff22fa199677bf9ab5a33f8724
Eddie gikundi <59659920+eddiemg10@users.noreply.github.com>
2022-06-09 19:20:37 +0300

    Elib (#18)
    * adding forms admin e-lib module
    
    just that
    
    * Edit functionalities for all levels added plus the file level added together with edit and delete functionality
    
    Check it out😌
    
    Co-authored-by: Abbymuso1 <abby.muso@gmail.com>

--- app/Http/Controllers/ClassLevelController.php
+++ app/Http/Controllers/ClassLevelController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\ClassLevel;
 use Illuminate\Http\Request;
+use Psy\Readline\Hoa\Console;
 
 class ClassLevelController extends Controller
 {
@@ -11,9 +12,25 @@ class ClassLevelController extends Controller
     {
         $data = new ClassLevel();
         $data->school_level_id = $request->school_id;
-        $data->classlevel_name=$request->name;
+        $data->classlevel_name = $request->name;
         $data->save();
         return redirect()->back();
     }
 
+    public function update(Request $request, $id)
+    {
+        $data = ClassLevel::find($id);
+        $data->classlevel_name = $request->name;
+        $data->update();
+        return redirect()->back();
+    }
+
+    public function delete(Request $request)
+    {
+
+        $data = $request->input('data')['class1'];
+        $class = ClassLevel::find($data);
+        $class->delete();
+
+    }
 }
