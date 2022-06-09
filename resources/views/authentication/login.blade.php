14c67fc142eca44e407e414a2665c3d28b508c80
Adala Wanyande <awanyande@gmail.com>
2022-06-09 10:07:33 +0300

    Login and registration pages
    Login and registration pages

---
+++ resources/views/authentication/login.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <!--Esoma Logo-->
+        <div>
+
+        </div>
+        <!--Sign In Stuff-->
+        <div>
+            <!--Not Registered?-->
+            <div>
+                <P>Not Registered? <a href="">Create an account</a></P>
+            </div>
+
+        </div>
+    </body>
+</html>
