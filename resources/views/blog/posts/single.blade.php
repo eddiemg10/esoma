d3294aad837e64087cdbadffa20978e49b915777
Kendi Njeru <kendi.njeru@strathmore.edu>
2022-06-26 21:51:16 +0300

    Blog design
    Updated styling of the blog.

--- resources/views/blog/posts/single.blade.php
+++ resources/views/blog/posts/single.blade.php
@@ -7,18 +7,36 @@
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 	<script src="https://cdn.tailwindcss.com"></script>
 
+	<style>
+
+		strong{
+			width: 100vw;
+		}
+		
+		img{
+			width: 500px;
+			height: 500px;
+			border-radius: 10px;		
+		}
+
+		p{
+			padding: 10px 0px;
+		}
+	</style>
+
 	<title>Blogs</title>
 </head>
 
 <body>
 	<div class="flex flex-col p-20">
-		<h1 class="text-5xl text-blue-900 font-bold mb-20 text-center">{{ $post->title }}</h1>
-		<div>
-			<img src="{{asset('images/blog/'.$post->image)}}" alt="">
-
-
+		<h1 class="text-4xl text-blue-900 font-bold mb-10 text-center">{{ $post->title }}</h1>
+	
+		<div class="flex w-full justify-center">
+			<img class=" w-[500px] object-cover rounded-md justify-center ml-30" src="{{asset('images/blog/'.$post->image)}}" alt="">
 		</div>
-		<div class="p-20 px-40">
+		<br><br>
+
+		<div class="p-20 px-40 rounded-md border-2 border-black-100 blog-container flex flex-col  items-center text-lg">
 			{!! $post->content !!}
 		</div>
 	</div>
